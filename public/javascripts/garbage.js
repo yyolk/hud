35d0e5a6d0c86aa81dd5a4f85ece4d0f72295dc0
Joseph Yølk Chiocchi <joe@yolk.cc>
2012-11-15 19:59:27 -0600

    colors!

--- public/javascripts/garbage.js
+++ public/javascripts/garbage.js
@@ -1,19 +1,36 @@
-function genGarbage(){
+function genGarbage(count){
 
   var key ="", random;
   var letters = "abcdefghijklmnopqrstuvwxyz ";
-  random = Math.floor(Math.random() * letters.length);
-  key += letters.charAt(random);
+  letters+= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  letters+= "{}[],./?<>()'!@#$%^&*";
+  for (var i = 0; i<count; i++){
+    if (i !== 0 && i % 7 === 0){
+      key+=" ";
+    }
+    random = Math.floor(Math.random() * letters.length);
+    key += letters.charAt(random);
+  }
   return key;
 }
 
 
 function showGarbage(){
 
-  var current = document.getElementById("miniwindow").innerHTML;
-  current += genGarbage();
-  document.getElementById("miniwindow").innerHTML = current;
+  var ckeys = ["one", "two", "three", "four", "five", "six"];
+  //var current = document.getElementById("miniwindow").innerHTML;
+  var miniwindow = document.getElementById("miniwindow");
+  var next = document.createElement("span");
+  next.innerHTML = "<span class='" + ckeys[Math.floor(Math.random() * ckeys.length)] + "'>" + genGarbage(Math.floor(Math.random()*70)) + "</span>";
+  miniwindow.appendChild(next);
+  //current += "<span class='" + ckeys[Math.floor(Math.random() * ckeys.length)] + "'>" + next + "</span>";
+  //document.getElementById("miniwindow").innerHTML = current;
 
 }
 
-setInterval("showGarbage()", 10);
+function scrollBottom(){
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+setInterval("showGarbage()", 100);
+setInterval("scrollBottom()", 150);
